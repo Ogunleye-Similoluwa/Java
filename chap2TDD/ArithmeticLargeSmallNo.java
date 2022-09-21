6a6c5a16f790a921c86d456aad3d77d664f02cc7
Simi <osimiloluwa9@gmail.com>
2022-09-21 14:46:49 -0700

    main

---
+++ chap2TDD/ArithmeticLargeSmallNo.java
@@ -0,0 +1,30 @@
+package chap2.chap2TDD;
+
+public class ArithmeticLargeSmallNo {
+
+    public int sum(int num1 , int num2 , int num3 ) {
+        return num1 +num2 + num3;
+    }
+
+    public int average( int num1 , int num2 ,int num3) {
+        return ( num1 +num2 + num3)/3 ;
+    }
+
+    public int product(int num1 , int num2, int num3){
+        return num1 * num2 * num3;
+    }
+
+    public int largestNo1(int num1, int num2 , int num3) {
+
+        if (num2> num1) {
+            num2 = num1;
+
+        }
+            return num2;
+    }
+
+    public boolean largestNo2(int num1, int num2, int num3) {
+        if ((num2 > num1 && num2 > num3));
+        return true;
+    }
+}
