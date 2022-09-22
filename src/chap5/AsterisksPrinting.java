63dd8ae2640b2adaec5023ffafc4f79dc4eb1e9b
Simi <osimiloluwa9@gmail.com>
2022-09-22 12:01:31 -0700

    update

---
+++ src/chap5/AsterisksPrinting.java
@@ -0,0 +1,17 @@
+package chap5;
+
+import java.util.Scanner;
+
+public class AsterisksPrinting {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number");
+        int number = in.nextInt();
+
+        for (int i = 1 ; i <= number; i++){
+            System.out.print(" * ");
+        }
+
+
+    }
+}
